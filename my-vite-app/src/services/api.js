608cab6d56e69c0297585ccda5490b46708c53c4
nhatled18 <nhatled18@gmail.com>
2025-10-13 11:24:07 +0700

    update timeout from 30000ms to 60000ms

--- my-vite-app/src/services/api.js
+++ my-vite-app/src/services/api.js
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_URL
 
 const api = axios.create({
   baseURL: API_URL,
-  timeout: 30000,
+  timeout: 60000,
   headers: {
     'Content-Type': 'application/json',
   }
